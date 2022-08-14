3bf330b8d9e15ecbf83b9d8ec93ccc7e9d079704
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-14 22:45:37 +0700

    add task 16 solution

--- Lesson_002/Program.cs
+++ Lesson_002/Program.cs
@@ -51,17 +51,39 @@
 // Задача 14. Напишите программу, которая принимает на вход число и 
 // проверяет, кратно ли оно однjвременно 7 и 23.
 
-Console.Clear();
-Console.Write("Введите число:");
-int num = Convert.ToInt32(Console.ReadLine());
-int div1 = 7;
-int div2 = 23;
+// Console.Clear();
+// Console.Write("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int div1 = 7;
+// int div2 = 23;
 
-if (num % div1 == 0 | num % div2 == 0)
+// if (num % div1 == 0 | num % div2 == 0)
+// {
+//     Console.WriteLine($"Число {num} кратно {div1} и {div2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num} не кратно {div1} и {div2}");
+// }
+
+// Задача 16. Напишите программу, которая принимает на вход два числа и 
+// проверяет, является ли одно число квадратом другого.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
 {
-    Console.WriteLine($"Число {num} кратно {div1} и {div2}");
+    Console.Write($"Число {num1} является квадратом числа {num2}");
+}
+else if (num2 == num1 * num1)
+{
+    Console.Write($"Число {num2} является квадратом числа {num1}");
 }
 else
 {
-    Console.WriteLine($"Число {num} не кратно {div1} и {div2}");
+    Console.Write($"Числа {num1} и {num2} не являются квадратами друг друга.");
 }
