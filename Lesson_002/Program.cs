517ee99e62a5ff50682e5cbbb5f279a26e4363c5
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-14 22:06:42 +0700

    add task 12 solution

--- Lesson_002/Program.cs
+++ Lesson_002/Program.cs
@@ -20,10 +20,32 @@
 // Задача 11. Напишите программу, которая выводит случайное трёхзначное число 
 // и удаляет вторую цифру этого числа.
 
-Console.Clear();
-int num1 = new Random().Next(100, 999);
-Console.WriteLine($"Число = {num1}");
+// Console.Clear();
+// int num1 = new Random().Next(100, 999);
+// Console.WriteLine($"Число = {num1}");
 
-int fig1 = num1 / 100;
-int fig2 = num1 % 10;
-Console.WriteLine($"Число без второй цифры = {fig1}{fig2}");
+// int fig1 = num1 / 100;
+// int fig2 = num1 % 10;
+// Console.WriteLine($"Число без второй цифры = {fig1}{fig2}");
+
+// Задача 12. Напишите программу, которая будет принимать на вход два числа и 
+// выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток деления.
+
+Console.Clear();
+Console.Write("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 % num1  == 0)
+{
+    Console.Write($"Число {num2} кратно числу {num1}");
+}
+else
+{
+    int rem = num2 % num1;
+    Console.Write($"Число {num2} не кратно числу {num1}, остаток {rem}");
+}
+
+
