8d47cf10d6255d5367f0d8146fcc84b676cfded0
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-16 21:24:16 +0700

    add task 017 function

--- Lesson_003/Program.cs
+++ Lesson_003/Program.cs
@@ -1,31 +1,58 @@
-﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X=/0 и Y=/0 
+﻿//Задача 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X=/0 и Y=/0 
 // и выдаёт номер чеверти плоскости, в которой находится эта точка.
 
 Console.Clear();
 
-Console.Write ("Введите координаты точки Х не равные 0: ");
-int x = Convert.ToInt32 (Console.ReadLine());
+// Console.Write ("Введите координаты точки Х не равные 0: ");
+// int x = Convert.ToInt32 (Console.ReadLine());
+// Console.Write ("Введите координаты точки Y не равные 0: ");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-Console.Write ("Введите координаты точки Y не равные 0: ");
-int y = Convert.ToInt32(Console.ReadLine());
 // // моё решение
+
 // if (x > 0 && y > 0) Console.Write ("Первая четверть");
 // if (x < 0 && y > 0) Console.Write ("Вторая четверть");
 // if (x < 0 && y < 0) Console.Write ("Третья четверть");
 // if (x > 0 && y < 0) Console.Write ("Четвёртая четверть");
 // if (x == 0 || y == 0) Console.Write ("Ввели координаты равные 0. Повторите попытку.");
 
-//решение Александра
+// //решение Александра
 
-string msg = "";
+// string msg = "";
 
-switch (x, y)
+// switch (x, y)
+// {
+//     case (>0, >0): msg = "первая четверть"; break;
+//     case (<0, >0): msg = "вторая четверть"; break;
+//     case (<0, <0): msg = "третья четверть"; break;
+//     case (>0, <0): msg = "четвёртая четверть"; break;
+//     default: msg = "Ввели координаты раные 0"; break;
+// }
+// Console.WriteLine (msg);
+
+// решение через функцию(метод)
+
+void printQuarter (int a, int b)
 {
-    case (>0, >0): msg = "первая четверть"; break;
-    case (<0, >0): msg = "вторая четверть"; break;
-    case (<0, <0): msg = "третья четверть"; break;
-    case (>0, <0): msg = "четвёртая четверть"; break;
-    default: msg = "Ввели координаты раные 0"; break;
+if (a > 0 && b > 0) Console.Write ("Первая четверть"); 
+if (a < 0 && b > 0) Console.Write ("Вторая четверть");
+if (a < 0 && b < 0) Console.Write ("Третья четверть");
+if (a > 0 && b < 0) Console.Write ("Четвёртая четверть");
+if (a == 0 || b == 0) Console.Write ("Ввели координаты равные 0. Повторите попытку.");
 }
-Console.WriteLine (msg);
+
+Console.Write ("Введите координаты точки Х не равные 0: ");
+int x = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите координаты точки Y не равные 0: ");
+int y = Convert.ToInt32(Console.ReadLine());
+printQuarter(x, y);
+
+
+
+
+
+
+
+
+
 
