7552f904c0467f766c23fb27f15fdfb4a63b871c
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-17 21:29:33 +0700

    add Task 024

---
+++ Lesson_004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 24. Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А
+
+int GetSumNums(int number); // функция, "GetSumNumbers" - придуманное название.
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++) // int i -стартовое значение, условие, действие.
+    {
+       sum += i;                   // суммарное сохранение каждого значения при проходе цикла
+    }
+    return sum;                  // вывод итога, после завершения цикла
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр от 1 до {num} = {GetSumNums(num)}");
