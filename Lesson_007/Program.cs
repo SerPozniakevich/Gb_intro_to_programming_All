ccdc4b723c328126f7b1dfaab70f3d1ae45fad91
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-29 05:23:49 +0700

    add correct entering condition

--- Lesson_007/Program.cs
+++ Lesson_007/Program.cs
@@ -7,46 +7,46 @@
 //Сгенерировать двумерный массив со случайными числами, вывести его на экран,
 // оформить в виде функции (пригодьтся в дальнейшем)
 
-// int Prompt(string message)
-// {
-//     System.Console.Write(message);                    //Вывести сообщение
-//     int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
-//     return result;                                    // Возвращает результат
-// }
+int Prompt(string message)
+{
+    System.Console.Write(message);                    //Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
+    return result;                                    // Возвращает результат
+}
 
 
-// int[,] FillArray(int numLine, int numColumns, int maxRand = 20, int minRand = 0) //Функция заполнения матицы случайными числами, ограничено [0; 20)
-// {
-//     int[,] matrix = new int[numLine, numColumns];
-//     for (int i = 0; i < matrix.GetLength; i++)  //Строка
-//     {
-//         for (int j = 0; j < matrix.GetLength; j++) //Столбец
-//         {
-//             matrix[i, j] = new Random().Next(minRand, maxRand); //[0; 20) по умолчанию
-//         }
-//     }
-//     return matrix;
-// }
+int[,] FillArray(int numLine, int numColumns, int maxRand = 20, int minRand = 0) //Функция заполнения матицы случайными числами, ограничено [0; 20)
+{
+    int[,] matrix = new int[numLine, numColumns];
+    for (int i = 0; i < matrix.GetLength(0); i++)  //Строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //Столбец
+        {
+            matrix[i, j] = new Random().Next(minRand, maxRand); //[0; 20) по умолчанию
+        }
+    }
+    return matrix;
+}
 
 
-// void PrintArray(int[,]matrix)                        //функция печати массива
-// {
-//     for (int i = 0; i < matrix.GetLength; i++)       //Строка
-//     {
-//         for (int j = 0; j < matrix.GetLength; j++)   //Столбец
-//         {
-//             Console.Write($"{matrix[i, j]}\t");     // "\t" - символ обозначает табуляцию
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void PrintArray(int[,]matrix)                        //функция печати массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)       //Строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)   //Столбец
+        {
+            Console.Write($"{matrix[i, j]}\t");     // "\t" - символ обозначает табуляцию
+        }
+        Console.WriteLine();
+    }
+}
 
-// int numLine = Prompt("Введите количество строк массива ");
-// int numColumns = Prompt("Введите количество столбцов массива ");
-// int maxRand = Prompt("Введите верхнюю границу диапазона "); //необязательно
-// int minRand = Prompt("Введите верхнюю границу диапазона "); //необязательно
-// int [,] matrix = FillArray(numLine, numColumns, maxRand, minRand); //Диапазон значений можно не у
-// PrintArray(matrix);
+int numLine = Prompt("Введите количество строк массива ");
+int numColumns = Prompt("Введите количество столбцов массива ");
+int maxRand = Prompt("Введите верхнюю границу диапазона "); //необязательно
+int minRand = Prompt("Введите верхнюю границу диапазона "); //необязательно
+int [,] matrix = FillArray(numLine, numColumns, maxRand, minRand); //Диапазон значений можно не у
+PrintArray(matrix);
 
 
 //Задайте двумерный массив размера m на n, каждый элемент в массиве
@@ -202,55 +202,55 @@
 
 //Введённый элемент: 6, результат: такого элемента нет.
 
-Random rand = new Random();
+// Random rand = new Random();
 
-void FillMatrix (int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            matr[i, j] = rand.Next(1, 15);
-        }
-    }
-}
+// void FillMatrix (int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = rand.Next(1, 15);
+//         }
+//     }
+// }
 
-void PrintArray(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-       for (int j = 0; j < arr.GetLength(1); j++)
-       {
-            Console.Write($"{arr[i, j]}\t");
-       } 
-       System.Console.WriteLine();
-    }
-}
+// void PrintArray(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//        for (int j = 0; j < arr.GetLength(1); j++)
+//        {
+//             Console.Write($"{arr[i, j]}\t");
+//        } 
+//        System.Console.WriteLine();
+//     }
+// }
 
 
 
-int[,] matrix = new int[3, 3];
+// int[,] matrix = new int[3, 3];
 
-FillMatrix(matrix);
-PrintArray(matrix);
+// FillMatrix(matrix);
+// PrintArray(matrix);
 
-System.Console.WriteLine("Input a number: ");
-int element = int.Parse(Console.ReadLine());
-FindElem(matrix, element);
+// System.Console.WriteLine("Input a number: ");
+// int element = int.Parse(Console.ReadLine());
+// FindElem(matrix, element);
 
-void FindElem (int[,] matrix, int element)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-           if (element == matrix[i, j])
-           {
-             System.Console.WriteLine($"Введённое число {element} находится на позиции {i},{j} ");
-             return;
-           }
-        }
-    }
-    System.Console.WriteLine("Введённого элемента нет в массиве.");
+// void FindElem (int[,] matrix, int element)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//            if (element == matrix[i, j])
+//            {
+//              System.Console.WriteLine($"Введённое число {element} находится на позиции {i},{j} ");
+//              return;
+//            }
+//         }
+//     }
+//     System.Console.WriteLine("Введённого элемента нет в массиве.");
 
-}
+// }
