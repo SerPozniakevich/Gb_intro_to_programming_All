f901f5a10b764127fecdd95d98ac7661f97b7570
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-28 20:58:03 +0700

    add solution SQR

--- Lesson_007/Program.cs
+++ Lesson_007/Program.cs
@@ -1,4 +1,9 @@
-﻿// НЕОБХОДИМА ДОРАБОТКА!!!
+﻿// int Prompt(string messadge) - функция Prompt берёт в качестве параметра какое-то сообщение
+//
+
+
+
+// НЕОБХОДИМА ДОРАБОТКА!!!
 //Сгенерировать двумерный массив со случайными числами, вывести его на экран,
 // оформить в виде функции (пригодьтся в дальнейшем)
 
@@ -47,6 +52,45 @@
 //Задайте двумерный массив размера m на n, каждый элемент в массиве
 // находится по формуле: A = m+n. Выведете полученный массив на экран.
 
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+
+// int[,] FillArray(int numLine, int numColumns)
+// {
+//     int[,] matrix = new int[numLine, numColumns];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = i + j; // Заполнение массива по формуле А = m +n
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//        for (int j = 0; j < matrix.GetLength(1); j++)
+//        {
+//             Console.Write($"{matrix[i,j]}\t");
+//        } 
+//        Console.WriteLine();
+//     }
+// }
+
+// int[,]array = FillArray(Prompt("Введите число строк > "), Prompt("Введите число колонок > "));
+// PrintArray(array);
+
+
+//Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+
 int Prompt(string message)
 {
     System.Console.Write(message);
@@ -54,30 +98,49 @@ int Prompt(string message)
     return result;
 }
 
-int[,] FillArray(int numLine, int numColumns)
+int[,] FillArray(int numLine, int numColumns, int maxRand = 20, int minRand = 0)
 {
-    int[,] matrix = new int[numLine, numColumns];
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    int[,] matrix = new int[numLine, numColumns];//Создание нового массива
+    for (int i = 0; i < matrix.GetLength(0); i++)//Условие для создания строки
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) //Условие для создания столбца
         {
-            matrix[i, j] = i + j; // Заполнение массива по формуле А = m +n
+            matrix[i, j] = new Random().Next(minRand, maxRand); // Заполнение массива по формуле А = m +n
         }
     }
-    return matrix;
+    return matrix; // Возвращение результата
 }
 
-void PrintArray(int[,] matrix)
+void PrintArray(int[,] matrix) //Печать массива
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
        for (int j = 0; j < matrix.GetLength(1); j++)
        {
-            Console.Write($"{matrix[i,j]}\t");
+            Console.Write($"{matrix[i,j]}\t"); //Заполнение строки (вывод очередной строки)
        } 
-       Console.WriteLine();
+       Console.WriteLine(); //переход на следущую строку
     }
 }
 
-int[,]array = FillArray(Prompt("Введите число строк > "), Prompt("Введите число колонок > "));
-PrintArray(array);
+
+int[,] ConvertToSQR(int [,] array) //Функция для замены значений с чётными строками и столбцами на их квадрат
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j= 0; j< array.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0) // 
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+int[,] massiv = FillArray(5, 5); //Объявляем массив "massiv", и указываем по какой функции он будет заполняться, указываем размер
+PrintArray(massiv); // выводим на экран полученное
+System.Console.WriteLine(); //Печать пустой строки
+int[,] newmassiv = ConvertToSQR(massiv); //объявление нового массива и запуск функции SQR
+PrintArray(newmassiv);// вывод результата на печать
